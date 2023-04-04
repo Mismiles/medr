4313dc6e42e60a3d6dc05e767e00ff9297d498c3
Msmiles <muhammadirfan123@hotmail.com>
2023-04-04 13:46:40 +0000

    added toggle to textboxes upto and including refferal to pharmacist/technician

---
+++ script.js
@@ -0,0 +1,114 @@
+//Show and hide allergybox//
+
+function show() {
+    var x = document.getElementById("AllergyBox");
+    if (x.style.display = "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function hide() {
+    var x = document.getElementById("AllergyBox");
+    if (x.style.display = "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
+//Show and hide reg-meds box//
+  function show1() {
+    var x = document.getElementById("reg-meds");
+    if (x.style.display = "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function hide1() {
+    var x = document.getElementById("reg-meds");
+    if (x.style.display = "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
+//Show and hide acute-meds box//
+function show2() {
+    var x = document.getElementById("acute-meds");
+    if (x.style.display = "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function hide2() {
+    var x = document.getElementById("acute-meds");
+    if (x.style.display = "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
+  //Show and hide otc-meds box//
+function show3() {
+    var x = document.getElementById("otc-box");
+    if (x.style.display = "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function hide3() {
+    var x = document.getElementById("otc-box");
+    if (x.style.display = "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
+//Show and hide smoking box//
+function show4() {
+    var x = document.getElementById("smoking-box");
+    if (x.style.display = "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function hide4() {
+    var x = document.getElementById("smoking-box");
+    if (x.style.display = "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
+//Show and hide referral box//
+function show5() {
+    var x = document.getElementById("referral-box");
+    if (x.style.display = "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function hide5() {
+    var x = document.getElementById("referral-box");
+    if (x.style.display = "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
