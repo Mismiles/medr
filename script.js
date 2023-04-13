ce8218841ece1bba7b043c9e879972d5eac9fd69
Msmiles <muhammadirfan123@hotmail.com>
2023-04-13 08:16:37 +0000

    corrected speeling of patient, changed colours of sources, attempted onclick event to highlight button

--- script.js
+++ script.js
@@ -1,2 +1,5 @@
 //Show and hide toggle for doctors review, allergybox, regular meds, acute meds, otc meds, smoking, technician referral, refferal, dosette box, anticoag,     //
 const toggle=(id,bool=false)=>document.getElementById(id).style.display= bool? 'block': 'none'
+
+//Highlight source button onclick     //
+const highlight=(id,bool=false)=>document.getElementById(id).className = bool? 'btn btn-secondary': 'btn btn-outline-secondary'
